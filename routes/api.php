f61c21e31bc964b8a2b1de43375403237ca3dc1f
Valeriy <valera.komar.1999@gmail.com>
2024-09-06 13:29:06 +0300

    Add product resource and API routes

---
+++ routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::apiResource('products', ProductController::class);
